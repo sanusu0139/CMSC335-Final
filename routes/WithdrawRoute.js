ba7f48551f1b2b81d7b81dd67500338d51fd5ac7
sanusu0139 <102300719+sanusu0139@users.noreply.github.com>
2025-05-18 17:05:39 -0400

    Update WithdrawRoute.js

--- routes/WithdrawRoute.js
+++ routes/WithdrawRoute.js
@@ -35,6 +35,12 @@ router.post("/", async(req, res) => {
             }     
         }
         pokeTable += '</table>';
+        if (!pokemon) {
+            return res.render("Withdraw", {
+                availablePokemon: pokeTable,
+                errorMessage: "This pokemon does not exist. Please choose a real pokemon."
+            });
+        }
         if(pokemon && equipCount < 6 && pokemon.inBox && !pokemon.equipped){
                     await PokemonModel.updateOne(
                         { name: pokemonName },
@@ -46,9 +52,8 @@ router.post("/", async(req, res) => {
             return res.render("Withdraw", {availablePokemon: pokeTable, errorMessage: "This pokemon is not in your box. Please deposit it before withdrawing." });
         }else if(pokemon.equipped){
             return res.render("Withdraw", {availablePokemon: pokeTable, errorMessage: "This pokemon is already equipped. Please withdraw a different pokemon." });
-        }else{
-            return res.render("Withdraw", {availablePokemon: pokeTable, errorMessage: "This pokemon does not exist. Please choose a real pokemon." });
         }
+        
         return res.render("Withdraw", {availablePokemon: pokeTable, errorMessage: null});
     }catch(error){
         console.error(error);
